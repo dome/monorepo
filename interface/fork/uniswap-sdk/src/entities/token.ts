55dde9bb1f44659b993fea5bfb41f1f50bd5ef8e
Poswap Panda <71455648+Poswap@users.noreply.github.com>
2020-10-22 15:36:05 +0700

    Update token.ts

--- interface/fork/uniswap-sdk/src/entities/token.ts
+++ interface/fork/uniswap-sdk/src/entities/token.ts
@@ -81,6 +81,6 @@ export const WETH = {
   [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
   [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether'),
   [ChainId.TCH]: new Token(ChainId.TCH, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WTCH', 'Wrapped Ether'),
-  [ChainId.BSC_MAINNET]: new Token(ChainId.BSC_MAINNET, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WBNB', 'Wrapped BNB'),
+  [ChainId.BSC_MAINNET]: new Token(ChainId.BSC_MAINNET, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB'),
   [ChainId.BSC_TESTNET]: new Token(ChainId.BSC_TESTNET, '0x68EC3f4D5EDb411362DcE5aa64e0ACEd4cb33Dcc', 18, 'WBNB', 'Wrapped BNB')
 }
